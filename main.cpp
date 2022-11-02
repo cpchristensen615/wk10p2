1b5d001236a453bd7d01e4024bd702401c092e8e
cpchristensen <112569319+cpchristensen615@users.noreply.github.com>
2022-11-02 00:30:23 +0000

    almost works with structs

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-  int i = 0;
-  for (int i = 0; i < 1000; i++){
+  struct group
+  {
     string month;
     int day;
     int year;
@@ -13,23 +13,33 @@ int main() {
     char x;
     int fHours;
     int fMins;
+  };
+  int n = 1000;
+  group entries[n];
+  n = 0;
+  
+  while(cin){
+    cin >> entries[n].month >> entries[n].day >> entries[n].year >> entries[n].iHours >> entries[n].x >> entries[n].iMins >> entries[n].fHours >> entries[n].x >> entries[n].fMins;
+    n++;
+  }
+  
+  
+  for (int i = 0; i < n; i++){
     int riseMin;
     int setMin;
     int netMin;
     int dayHours;
     int dayMin;
   
-    cin >> month >> day >> year >> iHours >> x >> iMins >> fHours >> x >> fMins;
-    
-    riseMin = (60*iHours) + iMins;
-    setMin = (60*fHours) + fMins;
+    riseMin = (60*entries[i].iHours) + entries[i].iMins;
+    setMin = (60*entries[i].fHours) + entries[i].fMins;
     
     netMin = setMin - riseMin;
     
     dayHours = netMin / 60;
     dayMin = netMin % 60;
   
-    cout << month << " " << day << " " << year << " " << dayHours << " hours " <<          dayMin << " minutes" << endl;
+    cout << entries[i].month << " " << entries[i].day << " " << entries[i].year << " " << dayHours << " hours " <<          dayMin << " minutes" << endl;
   }
   return 0;
 }
